8ad0a50a2a55a600d5ac602d4d27209e36a54425
sajjanaml <laxmansajjanam24@gmail.com>
2022-01-28 06:36:45 -0500

    Operator Precedence

---
+++ src/com/pluralsight/variables_datatypes_mathoperators/MathOrder.java
@@ -0,0 +1,17 @@
+package com.pluralsight.variables_datatypes_mathoperators;
+
+public class MathOrder {
+    public static void main(String[] args) {
+        int v1=21;
+        int v2=6;
+        int v3=3;
+        int v4=1;
+
+        int r1=v1-v2/v3;
+        int r2=(v1-v2)/v3;
+
+        System.out.println(r1);
+        System.out.println(r2);
+
+    }
+}
